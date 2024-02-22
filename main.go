f576902c4aa785c1dca756f1436cddaaf7399208
Chris Gianelloni <wolf31o2@blinklabs.io>
2024-02-21 20:19:01 -0500

    feat: build client (#7)
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>

--- main.go
+++ main.go
@@ -4,22 +4,30 @@ import (
 	"net/http"
 
 	"connectrpc.com/connect"
+	"github.com/utxorpc/go-codegen/utxorpc/v1alpha/build/buildconnect"
 	"github.com/utxorpc/go-codegen/utxorpc/v1alpha/sync/syncconnect"
 )
 
 type UtxorpcClient struct {
-	httpClient connect.HTTPClient
-	baseUrl    string
-	ChainSync  syncconnect.ChainSyncServiceClient
+	httpClient  connect.HTTPClient
+	baseUrl     string
+	ChainSync   syncconnect.ChainSyncServiceClient
+	LedgerState buildconnect.LedgerStateServiceClient
 }
 
 func NewClient(httpClient *http.Client, baseUrl string) UtxorpcClient {
 	var client UtxorpcClient
 	chainSyncClient := syncconnect.NewChainSyncServiceClient(httpClient, baseUrl, connect.WithGRPC())
+	ledgerStateClient := buildconnect.NewLedgerStateServiceClient(httpClient, baseUrl, connect.WithGRPC())
 	client.httpClient = httpClient
 	client.baseUrl = baseUrl
 	client.ChainSync = chainSyncClient
+	client.LedgerState = ledgerStateClient
 	return client
+}
+
+func (u *UtxorpcClient) HTTPClient() connect.HTTPClient {
+	return u.httpClient
 }
 
 func (u *UtxorpcClient) URL() string {
