57a625a8992bc8a98e70244116eb009197a0872f
Chris Gianelloni <wolf31o2@blinklabs.io>
2024-02-22 07:43:40 -0500

    feat: submit client
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"connectrpc.com/connect"
 	"github.com/utxorpc/go-codegen/utxorpc/v1alpha/build/buildconnect"
 	"github.com/utxorpc/go-codegen/utxorpc/v1alpha/sync/syncconnect"
+	"github.com/utxorpc/go-codegen/utxorpc/v1alpha/submit/submitconnect"
 )
 
 type UtxorpcClient struct {
@@ -13,16 +14,19 @@ type UtxorpcClient struct {
 	baseUrl     string
 	ChainSync   syncconnect.ChainSyncServiceClient
 	LedgerState buildconnect.LedgerStateServiceClient
+	Submit      submitconnect.SubmitServiceClient
 }
 
 func NewClient(httpClient *http.Client, baseUrl string) UtxorpcClient {
 	var client UtxorpcClient
 	chainSyncClient := syncconnect.NewChainSyncServiceClient(httpClient, baseUrl, connect.WithGRPC())
 	ledgerStateClient := buildconnect.NewLedgerStateServiceClient(httpClient, baseUrl, connect.WithGRPC())
+	submitClient := submitconnect.NewSubmitServiceClient(httpClient, baseUrl, connect.WithGRPC())
 	client.httpClient = httpClient
 	client.baseUrl = baseUrl
 	client.ChainSync = chainSyncClient
 	client.LedgerState = ledgerStateClient
+	client.Submit = submitClient
 	return client
 }
 
