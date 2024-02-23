15fe518167dd478d5d49976b1ac6888aa3eb7c34
Chris Gianelloni <wolf31o2@blinklabs.io>
2024-02-23 09:05:18 -0500

    feat: watch client (#9)
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/utxorpc/go-codegen/utxorpc/v1alpha/build/buildconnect"
 	"github.com/utxorpc/go-codegen/utxorpc/v1alpha/sync/syncconnect"
 	"github.com/utxorpc/go-codegen/utxorpc/v1alpha/submit/submitconnect"
+	"github.com/utxorpc/go-codegen/utxorpc/v1alpha/submit/watchconnect"
 )
 
 type UtxorpcClient struct {
@@ -15,6 +16,7 @@ type UtxorpcClient struct {
 	ChainSync   syncconnect.ChainSyncServiceClient
 	LedgerState buildconnect.LedgerStateServiceClient
 	Submit      submitconnect.SubmitServiceClient
+	Watch       watchconnect.WatchServiceClient
 }
 
 func NewClient(httpClient *http.Client, baseUrl string) UtxorpcClient {
@@ -22,11 +24,13 @@ func NewClient(httpClient *http.Client, baseUrl string) UtxorpcClient {
 	chainSyncClient := syncconnect.NewChainSyncServiceClient(httpClient, baseUrl, connect.WithGRPC())
 	ledgerStateClient := buildconnect.NewLedgerStateServiceClient(httpClient, baseUrl, connect.WithGRPC())
 	submitClient := submitconnect.NewSubmitServiceClient(httpClient, baseUrl, connect.WithGRPC())
+	watchClient := watchconnect.NewWatchServiceClient(httpClient, baseUrl, connect.WithGRPC())
 	client.httpClient = httpClient
 	client.baseUrl = baseUrl
 	client.ChainSync = chainSyncClient
 	client.LedgerState = ledgerStateClient
 	client.Submit = submitClient
+	client.Watch = watchClient
 	return client
 }
 
