b7342331d02ea77c16e63aa2da8da91422c14eee
Ales Verbic <verbotenj@blinklabs.io>
2024-07-02 17:15:08 -0400

    feat: add submit example with refactor of common functions (#30)
    Signed-off-by: Ales Verbic <verbotenj@blinklabs.io>

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package sdk
 
 import (
+	"crypto/tls"
+	"errors"
+	"fmt"
+	"net"
 	"net/http"
 
 	"connectrpc.com/connect"
@@ -8,29 +12,40 @@ import (
 	"github.com/utxorpc/go-codegen/utxorpc/v1alpha/submit/submitconnect"
 	"github.com/utxorpc/go-codegen/utxorpc/v1alpha/sync/syncconnect"
 	"github.com/utxorpc/go-codegen/utxorpc/v1alpha/watch/watchconnect"
+	"golang.org/x/net/http2"
 )
 
 type UtxorpcClient struct {
 	httpClient connect.HTTPClient
 	baseUrl    string
+	headers    map[string]string
 	ChainSync  syncconnect.ChainSyncServiceClient
 	Query      queryconnect.QueryServiceClient
 	Submit     submitconnect.SubmitServiceClient
 	Watch      watchconnect.WatchServiceClient
 }
 
-func NewClient(httpClient *http.Client, baseUrl string) UtxorpcClient {
-	var client UtxorpcClient
-	chainSyncClient := syncconnect.NewChainSyncServiceClient(httpClient, baseUrl, connect.WithGRPC())
-	queryClient := queryconnect.NewQueryServiceClient(httpClient, baseUrl, connect.WithGRPC())
-	submitClient := submitconnect.NewSubmitServiceClient(httpClient, baseUrl, connect.WithGRPC())
-	watchClient := watchconnect.NewWatchServiceClient(httpClient, baseUrl, connect.WithGRPC())
-	client.httpClient = httpClient
-	client.baseUrl = baseUrl
-	client.ChainSync = chainSyncClient
-	client.Query = queryClient
-	client.Submit = submitClient
-	client.Watch = watchClient
+type ClientOption func(*UtxorpcClient)
+
+func WithHeaders(headers map[string]string) ClientOption {
+	return func(client *UtxorpcClient) {
+		client.headers = headers
+	}
+}
+
+func NewClient(httpClient *http.Client, baseUrl string, options ...ClientOption) *UtxorpcClient {
+	client := &UtxorpcClient{
+		httpClient: httpClient,
+		baseUrl:    baseUrl,
+		ChainSync:  syncconnect.NewChainSyncServiceClient(httpClient, baseUrl, connect.WithGRPC()),
+		Query:      queryconnect.NewQueryServiceClient(httpClient, baseUrl, connect.WithGRPC()),
+		Submit:     submitconnect.NewSubmitServiceClient(httpClient, baseUrl, connect.WithGRPC()),
+		Watch:      watchconnect.NewWatchServiceClient(httpClient, baseUrl, connect.WithGRPC()),
+	}
+
+	for _, option := range options {
+		option(client)
+	}
 	return client
 }
 
@@ -40,4 +55,56 @@ func (u *UtxorpcClient) HTTPClient() connect.HTTPClient {
 
 func (u *UtxorpcClient) URL() string {
 	return u.baseUrl
+}
+
+func createHttpClient() *http.Client {
+	return &http.Client{
+		CheckRedirect: func(_ *http.Request, _ []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+		Transport: &http2.Transport{
+			AllowHTTP: true,
+			DialTLS: func(network, addr string, _ *tls.Config) (net.Conn, error) {
+				// If you're also using this client for non-h2c traffic, you may want
+				// to delegate to tls.Dial if the network isn't TCP or the addr isn't
+				// in an allowlist.
+				return net.Dial(network, addr)
+			},
+		},
+	}
+}
+
+func CreateUtxoRPCClient(baseUrl string, options ...ClientOption) *UtxorpcClient {
+	httpClient := createHttpClient()
+	return NewClient(httpClient, baseUrl, options...)
+}
+
+func (u *UtxorpcClient) SetHeader(key, value string) {
+	if u.headers == nil {
+		u.headers = make(map[string]string)
+	}
+	u.headers[key] = value
+}
+
+func (u *UtxorpcClient) SetHeaders(headers map[string]string) {
+	u.headers = headers
+}
+
+func (u *UtxorpcClient) RemoveHeader(key string) {
+	delete(u.headers, key)
+}
+
+func (u *UtxorpcClient) AddHeadersToRequest(req connect.AnyRequest) {
+	for key, value := range u.headers {
+		req.Header().Set(key, value)
+	}
+}
+
+func HandleError(err error) {
+	fmt.Println(connect.CodeOf(err))
+	if connectErr := new(connect.Error); errors.As(err, &connectErr) {
+		fmt.Println(connectErr.Message())
+		fmt.Println(connectErr.Details())
+	}
+	panic(err)
 }
